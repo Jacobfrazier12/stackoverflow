879e1098a98b03e0741d9735db81b20634f13ec8
jacobfrazier12 <jacobfrazier12@gmail.com>
2023-05-15 06:06:47 -0400

    Added thread sleeper.

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
+from time import time
+from os import getcwd, path
 from zipfile import ZipFile
 from pandas import DataFrame
 from requests import get, exceptions
 import matplotlib as plt
 
 #This section downloads all the StackOverflow survey results.
-url = "https://stackoverflow.com/survey/"
+
+
+
 for year in range(2011, 2023):
     try:
-        data = get(url+str(year), stream = True, timeout = 10)
-        data.raise_for_status()
-        with open(f"results_{year}.zip","wb" ) as f:
-            for chunk in data.iter_content(chunk_size = 1024):
-                f.write(chunk)
+        url = f"https://stackoverflow.com/survey/{year}"
+        file_path = path.join(getcwd(), f"results_{year}.zip")
+        if not path.exists(file_path):
+            data = get(url, timeout=60)
+            data.raise_for_status()
+            with open(file_path,"wb" ) as f:
+                for data in data.content:
+                    f.write(data)
+            time.sleep(10)
+        
     except exceptions.RequestException as e:
         raise SystemExit(e)
    
